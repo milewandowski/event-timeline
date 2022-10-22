88150f7a23cc559347f2c0065b10d04e2cdf0da6
milewandowski <m.21lewandowski@gmail.com>
2022-10-22 16:36:38 +0200

    Initial commit

---
+++ src/main/java/com/lew/eventtimeline/EventTimelineApplication.java
@@ -0,0 +1,13 @@
+package com.lew.eventtimeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTimelineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventTimelineApplication.class, args);
+    }
+
+}
